5bdc2ea451b1200d35ca2a7e4ff7059a6fa2ee13
fjofjoe <fjofjoe>
2026-06-21 09:58:39 +0800

    这是我的博客

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -6,10 +6,10 @@ const SITE_LANG = "zh_CN";
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "Firefly",
+	title: "Holy Bro",
 
 	// 站点副标题
-	subtitle: "Demo site",
+	subtitle: "Hello World!",
 
 	// 站点 URL
 	site_url: "https://firefly.cuteleaf.cn",
