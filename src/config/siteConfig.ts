4f14b1a8e0164c7151cad7c45740cb0f54e487ea
fjofjoe <2295696996@qq.com>
2026-06-21 16:05:13 +0800

    test

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -16,7 +16,7 @@ export const siteConfig: SiteConfig = {
 
 	// 站点描述
 	description:
-		"Firefly 是一款基于 Astro 框架和 Fuwari 模板开发的清新美观且现代化个人博客主题模板，专为技术爱好者和内容创作者设计。该主题融合了现代 Web 技术栈，提供了丰富的功能模块和高度可定制的界面，让您能够轻松打造出专业且美观的个人博客网站。",
+		"用了夏叶大佬的模版,一次尝试。",
 
 	// 站点关键词
 	keywords: [
@@ -27,6 +27,7 @@ export const siteConfig: SiteConfig = {
 		"博客",
 		"技术博客",
 		"静态博客",
+		"fjofjoe",
 	],
 
 	// 主题色
